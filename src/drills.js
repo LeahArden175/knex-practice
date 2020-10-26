f09797c77887824984470f68dd9b7b3ce97f1c5a
Leah Arden <leaharden175@gmail.com>
2020-10-26 16:05:04 -0400

    finished drills

--- src/drills.js
+++ src/drills.js
@@ -19,3 +19,48 @@ function searchByTerm(searchterm) {
 }
 
 searchByTerm('urger')
+
+
+function paginateProducts(pageNumber){
+    const productsPerPage = 6
+    const offset = productsPerPage * (pageNumber - 1)
+    knexInstance
+        .select('*')
+        .from('shopping_list')
+        .limit(productsPerPage)
+        .offset(offset)
+        .then(result => {
+            console.log(result)
+        })
+}
+
+paginateProducts(2)
+
+function addedAfterDate(daysAgo){
+    knexInstance
+        .select('*')
+        .from('shopping_list')
+        .where(
+            'date_added',
+            '>',
+            knexInstance.raw(`now() - '?? days':: INTERVAL`, daysAgo)
+          )
+        .then(results => {
+            console.log(results)
+        })  
+}
+
+addedAfterDate(3)
+
+function totalCategoryCost(){
+    knexInstance
+        .select('category')
+        .sum('price as total')
+        .from('shopping_list')
+        .groupBy('category')
+        .then(result => {
+            console.log(result)
+        })
+}
+
+totalCategoryCost();
