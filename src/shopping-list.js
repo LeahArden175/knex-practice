1e68efa2f6ad2e409cdadc253bb8c8d7274b8190
Leah Arden <leaharden175@gmail.com>
2020-10-27 15:52:49 -0400

    finished shopping-list.js

--- src/shopping-list.js
+++ src/shopping-list.js
@@ -7,4 +7,29 @@ const knexInstance = knex({
     connection: process.env.DB_URL,
 })
 
+ShoppingListService.getAllItems(knexInstance)
+    .then(items => console.log(items))
+    .then(() =>
+        ShoppingListService.insertItem(knexInstance, {
+            checked: false,
+            name: "banana",
+            price: "0.69",
+            date_added: new Date("2029-01-22T16:28:32.615Z"),
+            category: "Breakfast",
+        })
+    )
+    .then(newItem => {
+        console.log(newItem)
+        return ShoppingListService.updateItem(
+            knexInstance,
+            newItem.id,
+            {name: 'Updated name'}
+        ).then(() => ShoppingListService.getByIdI(knexInstance, newItem.id))
+    }) 
+    .then(item => {
+        console.log(item)
+        return ShoppingListService.deleteItem(knexInstance, item.id)
+    })   
+
+
 console.log(ShoppingListService.getAllItems())
