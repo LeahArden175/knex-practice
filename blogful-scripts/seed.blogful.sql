0339eb62dc4dc590e13237b798e39f0defae346e
Leah Arden <leaharden175@gmail.com>
2020-10-27 11:30:15 -0400

    added blogful-scripts

---
+++ blogful-scripts/seed.blogful.sql
@@ -0,0 +1,29 @@
+BEGIN;
+
+INSERT INTO blogful_articles
+    (title, date_published, content)
+
+VALUES    
+    ('Where is the dog?', '2016-01-16 4:12:19', 'This is about a lost dog'),
+    ('Where is the cat?', '2016-01-18 12:45:13', 'This is about a lost cat'),
+    ('Where is the rabbit?', '2016-01-20 12:30:12', 'This is about a lost rabbit'),
+    ('Hello over there!', '2019-10-03 23:24:34', 'This is about finding someone on the other side of the world'),
+    ('There''s a snake in my boot!', '2018-05-23 10:32:48', 'Classic story about toys thay come to life'),
+    ('ohwowcow', '2018-08-16 15:05:54', 'Yummy ice cream recipes'),
+    ('Better from scratch', '2020-01-28 7:10:34', 'Recipes to make foods from scratch'),
+    ('Cravings', '2016-01-20 5:30:22', 'Cookbook about snacks!'),
+    ('Planty things', '2018-07-10 2:00:34', 'Book about house plants'),
+    ('Aroid who?', '2019-01-16 13:03:10', 'Information about different aroids'),
+    ('How much?!', '2017-04-19 10:10:10', 'This is about the cost of healthcare around the world'),
+    ('Interior Design', '2019-03-16 8:03:10', 'Teach youself some basics of interior design'),
+    ('Woodwork', '2018-04-20 9:48:15', 'An intro to woodwork'),
+    ('The big comfy chair', '2019-07-20 19:42:18', 'Childrens book about learning'),
+    ('Spices make the difference', '2020-07-03 18:31:45', 'Teaches how to mix spices to create your own spice blends'),
+    ('Bagels for brunch', '2018-07-20 19:42:18', 'Classic story about a young woman making it in the real world'),
+    ('The zoom apocalypse', '2018-11-11 04:12-12', 'A story about how zoom changed the way we work'),
+    ('People everywhere, no mask in sight', '2018-11-11 04:12-12', 'About thw world before and during the pandemic'),
+    ('Here''s Leah', '2016-10-16 23:00-14', 'About Leah and her life'),
+    ('The last one!', '2017-11-21 06:22-01', 'Finally the last one');
+
+
+COMMIT;
